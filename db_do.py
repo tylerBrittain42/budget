062ddecf89404716a74a796fe3dbfa81047f0ad0
Tyler <tbrittain100@gmail.com>
2023-06-18 00:28:52 -0400

    implement relations(untested)

--- db_do.py
+++ db_do.py
@@ -7,14 +7,14 @@ import models
 
 def main():
     
-    commands = {'create':create_all, 'drop':drop_all, 'reset':reset, 'populate':add_data}
+    commands = {'create':create_all, 'drop':drop_all, 'reset':reset, 'populate':create_dummy_data}
 
     try:
         config = dotenv_values()
-        connection_string = f'mysql+mysqlconnector://{config["USERNAME"]}:{config["PASSWORD"]}@{config["HOST"]}:{config["PORT"]}/{config["DATABASE"]}'
+        connection_string = f'postgresql+psycopg2://{config["USERNAME"]}:{config["PASSWORD"]}@{config["HOST"]}:{config["PORT"]}/{config["DATABASE"]}'
         print(connection_string)
 
-        engine = create_engine(connection_string, echo=True)
+        engine = create_engine(connection_string, echo=False)
         commands[sys.argv[1]](models.Base.metadata,engine)
     except Exception as e:
         print('error')
@@ -28,13 +28,14 @@ def drop_all(meta: MetaData, engine: Engine) -> None:
 def create_all(meta: MetaData, engine: Engine) -> None:
     meta.create_all(engine)
 
-def add_data(meta: MetaData, engine: Engine):
-    print('NOT IMPLEMENTED')
+def create_dummy_data(meta: MetaData, engine: Engine) -> None:
+
+    print('dummy data created')
 
 def reset(meta: MetaData, engine: Engine):
     meta.drop_all(engine)
     meta.create_all(engine)
-
+    create_dummy_data(meta, engine)
 
 if __name__ == '__main__':
     main()
--- models.py
+++ models.py
@@ -22,6 +22,12 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(30))
 
+    income_sources: Mapped[list['Income']] = relationship(back_populates="user")
+    ratios: Mapped[list['Ratio']] = relationship(back_populates="owner")
+    subs: Mapped[list['Subscription']] = relationship(back_populates='')
+    utilities: Mapped[list['Utility']] = relationship(back_populates='')
+    line_items:Mapped[list['Line_item']] = relationship(back_populates='')
+
     def __repr__(self) -> str:
         return f'<user:{self.name}, id:{self.id}>'
 
@@ -31,7 +37,8 @@ class Income(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     date: Mapped[datetime.date]
     amount: Mapped[int]
-    u_id: Mapped[int] = mapped_column(primary_key=True)
+    u_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+    user: Mapped['User'] = relationship(back_populates="income_sources")
 
     def __repr__(self) -> str:
         return f'<income:{self.amount}, date:{self.date}>'
@@ -43,17 +50,22 @@ class Category(Base):
     name: Mapped[str] = mapped_column(String(30))
     type: Mapped[str] = mapped_column(String(30))
 
+    # don't think I need to connect the relationship back
     def __repr__(self) -> str:
         return f'<category:{self.name}, id:{self.id}>'
 
 
 class Ratio(Base):
     __tablename__ = 'ratio'
+
     # is the primary key line neccessary for this table?
     id: Mapped[int] = mapped_column(primary_key=True)
-    c_id: Mapped[int] 
-    u_id: Mapped[int] 
+    # c_id: Mapped[int] 
+    u_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
     ratio: Mapped[float] 
+    
+    owner: Mapped['User'] = relationship(back_populates='ratios')
+    type: Mapped['Category'] = relationship()
 
     def __repr__(self) -> str:
         return f'<ratio:{self.id} TBD>' #INCOMPLETE
@@ -62,11 +74,12 @@ class Ratio(Base):
 class Utility(Base):
     __tablename__ = 'utility'
     id: Mapped[int] = mapped_column(primary_key=True)
+    u_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
     name: Mapped[str] = mapped_column(String(30))
+    
     due_date: Mapped[datetime.date]
     cost: Mapped[float]
     paid: Mapped[bool]
-    u_id: Mapped[int] 
 
 
     def __repr__(self) -> str:
@@ -76,11 +89,12 @@ class Utility(Base):
 class Subscription(Base):
     __tablename__ = 'subscription'
     id: Mapped[int] = mapped_column(primary_key=True)
+    u_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
     name: Mapped[str] = mapped_column(String(30))
+
     due_date: Mapped[datetime.date]
     cost: Mapped[float]
     paid: Mapped[bool]
-    u_id: Mapped[int] 
 
 
     def __repr__(self) -> str:
@@ -89,16 +103,17 @@ class Subscription(Base):
 class Line_item(Base):
     __tablename__ = 'line_item'
     id: Mapped[int] = mapped_column(primary_key=True)
+    u_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
     name: Mapped[str] = mapped_column(String(30))
+
     date: Mapped[datetime.date]
     cost: Mapped[float]
     reason: Mapped[str] = mapped_column(String(30))
-    category_id: Mapped[int]
-    u_id: Mapped[int] 
+    # category_id: Mapped[int]
     
 
-    # def __repr__(self) -> str:
-
+    def __repr__(self) -> str:
+        return f'<line_item:{self.name} cost:{self.cost}'
 
 
 
